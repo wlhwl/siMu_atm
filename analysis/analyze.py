8379f0543f3ccff9219ef2fad17e317c48bf51fa
Weilun Huang <huangweilun@mail.sdu.edu.cn>
2024-03-29 19:29:48 +0800

    set observation box

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -1,21 +1,58 @@
+import math
 
 from config import *
 from utils import *
 import numpy as np
+from scipy import special
 
-if __name__=='__main__':
+def Simu_spectrum_weight(e,emin,emax,prim_num):
+    nor = np.log(emax/emin)
+    density_reciprocal = e
+    return nor*density_reciprocal/(emax-emin)/prim_num
+
+def calculate_dnde(e,rc,a,gamma,z):
+    dnde = a * np.power(e,-gamma-1) * np.exp(-e/(z*rc))# dnde = phi / e
+    return dnde
+
+def Weighter(e,emin,emax,prim_num):
+    spec_wei = Simu_spectrum_weight(e,emin,emax,prim_num)
+    para_table = [[120*10**3,7000,1.661,1],[4*10**6,150,1.4,1],[1.3*10**9,1.4,1.4,1]]
+    dnde=0
+    for i in range(0,3):
+        dnde += calculate_dnde(e,para_table[i][0],para_table[i][1],para_table[i][2],para_table[i][3])/1.5295/np.power(10.,-5)
+    return dnde*spec_wei*np.log(10)*e #/5000/5000 *np.power(e,2.6)  *e /area #*e/100000 #*np.power(e,2.6)
+
+if __name__ == '__main__':
+    ##corsika
     myset = GlobalSetting()
+    prim_num = np.sum(myset.corsika_samples.num_events_list)
+    #get particles
+    cor_particles = myset.corsika_samples.SelectedParticles
+    cor_particles = cor_particles.loc[cor_particles.z == 500]
+    # weight
+    cor_particles_w = weight(cor_particles,Weighter,prim_num,myset.corsika_samples.energy_range[0],myset.corsika_samples.energy_range[1])
+    ##mupage
+    mp_particles = myset.mupage_samples.muons
+    mp_weight = 1 / myset.mupage_samples.livetime / (math.pi * myset.mupage_samples.extended_can_radius**2)#
+
+
     plots = myset.plots
     variables = list(plots.keys())
-    particles = myset.corsika_samples.particles
-
     for var in variables:
         pc = plots.get(var)
-        to_draw = particles[var]
-        zenith_ang = np.arccos(-to_draw)
+        to_draw = cor_particles_w[var]
+
         bins = pc.bins if hasattr(pc, 'bins') else 30
-        hist, bins, _ = pc.ax.hist(zenith_ang, bins, histtype="step", linewidth=2)
-    
+
+        hist_raw, bin_raw = np.histogram(to_draw, bins)
+
+        hist_w, bin_w = np.histogram(to_draw, bins,weights=cor_particles_w['weight'])
+        hist_w = hist_w / np.diff(bin_w)
+        hist_mp, bin_mp = np.histogram(mp_particles['energy'],bins,weights=np.ones_like(mp_particles['energy'])*mp_weight)
+        hist_mp = hist_mp / np.diff(bin_mp)
+        # pc.ax.plot((bin_w[1:]+bin_w[:-1])/2,hist_w,linestyle='--',label='sim')
+        pc.ax.errorbar((bins[1:]+bins[:-1])/2,hist_w,yerr=(hist_w/np.sqrt(hist_raw)), label="p 1-100TeV")
+        pc.ax.plot((bins[1:]+bins[:-1])/2,hist_mp)
+        pc.ax.legend()
         pc.apply_settings()
         pc.savefig()
-
--- analysis/config.py
+++ analysis/config.py
@@ -1,55 +1,127 @@
+import numpy as np
 import pandas as pd
 import math
 import glob
 from utils import *
 # import yaml
-
-class CorsikaSamples:
-    def __init__(self, path_list, num_events_list, mc_power_index, energy_range, costh_range, sample_cylinder_surface, obs_level='sea_level', radius_cut = 2000) -> None:
+class MupageSamples:
+    def __init__(self, path_list, energy_range, costh_range, extended_can_radius) -> None:
         self.path_list = path_list
-        self.num_events_list = num_events_list
-        self.mc_power_index = mc_power_index
         self.energy_range = energy_range
         self.costh_range = costh_range
+        self.extended_can_radius = extended_can_radius
+        self.muons, self.livetime, self.livetime_error = self.load_muons()
+        self.num_showers = len(self.muons.index.unique())
+        self.define_plots_vars()
+
+    def load_muons(self):
+        muons = pd.DataFrame()
+        livetime, livetime_error2 = 0., 0.
+        total_shower = 0.
+        for fpath in self.path_list:
+            print('loading ', fpath)
+
+            """
+            get mc_events.json from an output of MUPAGE
+            """
+            with open(fpath, 'r') as file:
+                first_row = file.readline()
+                # get livetime & total n events
+                second_row = file.readline().split(":")[1].split()
+                livetime += float(second_row[0])
+                livetime_error2 += float(second_row[1])**2
+
+            # Read the file into a DataFrame
+            df = pd.read_csv(fpath,
+                             delim_whitespace=True,  # Use whitespace as the delimiter
+                             skiprows=2,  # Skip the first two lines
+                             names=['showerId', 'multiplicity', 'muonId', 'x', 'y', 'z', 'nx', 'ny', 'nz', 'energy', 'relativeT', 'pdgId']) # t in s and relativeT in ns
+            # ShowerId starts from 0
+            df['showerId'] = df['showerId'] + total_shower - 1
+            df['muonId'] -= 1
+            # intrisic bugs in MUPAGE
+            df.loc[df.multiplicity==1, 'nz'] *= -1
+
+            total_shower += len(df.showerId.unique())
+            #df = df[df['energy'] > 200]
+            #df = df[df['nz'] < -0.9]
+
+            # merge shower
+            muons = pd.concat([muons, df])
+        return muons.set_index('showerId'), livetime, livetime_error2**0.5
+    def define_plots_vars(self):
+        self.muons['R'] = np.linalg.norm(self.muons[['x', 'y']], axis=1)
+        self.muons['costh'] = -1*self.muons['nz']
+
+        self.shower_vars = pd.DataFrame(index=self.muons.index.unique())
+        self.shower_vars['showerEnergy'] = self.muons.energy.groupby('showerId').sum()
+        self.shower_vars['muonMultiplicity'] = self.muons.multiplicity.groupby('showerId').first()
+        self.shower_vars['timeDuration'] = self.muons.relativeT.groupby('showerId').max() - self.muons.relativeT.groupby('showerId').min()
+
+class CorsikaSamples:
+    def __init__(self, path_list, josn_list, num_events_list, energy_range, costh_range, sample_cube_surface, select_id, obs_level='det_level') -> None:
+        self.path_list = path_list
+        self.json_list = josn_list
+        self.num_events_list = num_events_list
+        self.energy_range = energy_range
         self.costh_range = costh_range
-        self.sample_cylinder_surface = sample_cylinder_surface
-        self.radius_cut = radius_cut
-        
-        if (obs_level!='sea_level') and (obs_level!='det_level'):
-            raise Exception('obs_level should be either "sea_level" or "det_level", not {}'.format(obs_level))
+        self.sample_cube_surface = sample_cube_surface
         self.obs_level = obs_level
         self.pars_paths = [p + 'particles_' + obs_level + '/particles.parquet' for p in self.path_list]
-        self.prim_paths = [p + 'primary_det' for p in self.path_list]
         self.particles = self.load_pars_and_prim()
+        self.SelectedParticles = self.pick_up_particles(select_id)
+        self.primaries = self.get_prims()
 
     def load_pars_and_prim(self):
         particles = []
         shower_id_start = 0
         for ibatch in range(len(self.pars_paths)):
             cur_pars = pd.read_parquet(self.pars_paths[ibatch])
+            prim = pd.read_json(self.json_list[ibatch])
+            merge = cur_pars.merge(prim, on='shower', how='left')
+            cur_pars['E'] = merge['E']
             cur_pars.shower += shower_id_start
             shower_id_start += self.num_events_list[ibatch]
             particles.append(cur_pars)
-            # with open(self.prim_paths[ievent]) as f:
-            #     cur_prim = yaml.safe_load(f)
-            #     cur_prim = pd.DataFrame.from_dict(cur_prim)
         return pd.concat(particles)
+
+    def get_prims(self):
+        prims=[]
+        shower_id_start = 0
+        for ibatch in range(len(self.json_list)):
+            cur_prims = pd.read_json(self.json_list[ibatch])
+            cur_prims.shower += shower_id_start
+            shower_id_start += self.num_events_list[ibatch]
+            prims.append(cur_prims)
+        return pd.concat(prims)
+
+    def pick_up_particles(self, id):
+         selected = self.particles[self.particles['pdg'].isin(id)]
+         return selected
+
+def weight(particles, weighter, prim_num, emin, emax):
+    particles['weight'] = particles['E'].apply(weighter, args=(emin,emax,prim_num))
+    return particles
 
 def setup_plots(save_dir:str, plots:dict=None):
     if plots==None:
         plots = {}
-    plots['nz'] = PlotContainer(xlabel='zenith angle', ylabel='Counts', logx=False, logy=False, figname=save_dir + 'muon_zenith_ang_distribution.pdf', bins=np.linspace(0, 3.14, 21))
+    #plots['nz'] = PlotContainer(xlabel=r'$cos \theta$', ylabel=r'$dN/(dE\cdot dcos\theta)$', logx=False, logy=True, figname=save_dir + 'primaries_zenith_distribution.jpg', bins=np.linspace(0,1,50))
+    plots['kinetic_energy'] = PlotContainer(xlabel=r'$E \mu$', ylabel=r'$EdN/dE$', logx=True, logy=True, figname=save_dir + 'muon_e_distribution.jpg', bins=np.logspace(2,5,30))
     return plots
 
-
 class GlobalSetting:
-    path_list = glob.glob("/lustre/neutrino/huangweilun/atmos_muon/COR_atm_muon/test_proton/out_no_sea_level_obs/part?/my_shower/")
-    corsika_samples = CorsikaSamples(path_list=path_list, num_events_list=[1000 for i in path_list], mc_power_index=-2,\
-             energy_range=[1e9, 1e10], costh_range=[0.9, 1], sample_cylinder_surface=math.pi*2000**2)
-    
-    save_dir = './save/'
-    plots = setup_plots(save_dir)
+    cor_path_list = glob.glob("/media/ineffablord/T7/siMu_atm/data/p/out_box_1-100T_full_sky/part?*/my_shower/")
+    cor_json_list = glob.glob("/media/ineffablord/T7/siMu_atm/data/p/out_box_1-100T_full_sky/part?*/Primaries.json")
+    corsika_samples = CorsikaSamples(path_list=cor_path_list,  josn_list=cor_json_list,
+                                     num_events_list=[5000 for i in cor_path_list],
+                                     energy_range=[1e2, 1e5], costh_range=[0, 1], sample_cube_surface=5000*16000,
+                                     select_id=[13,-13])
+    mp_path_list = glob.glob("/media/ineffablord/T7/siMu_atm/data/mupage/evt/*.evt")
+    mupage_samples = MupageSamples(path_list=mp_path_list, energy_range=(1, 1e5), costh_range=(0,math.cos(85)), extended_can_radius=300)
 
+    save_dir = './test_weight/'
+    plots = setup_plots(save_dir)
 
 if __name__=='__main__':
     myset = GlobalSetting()
