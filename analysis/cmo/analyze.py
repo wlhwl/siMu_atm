55acad1d0d7b9cf9d13481e866704a3b25440af2
cmo <fourty2@126.com>
2024-04-07 15:42:24 +0800

    Draw muon costh

--- analysis/cmo/analyze.py
+++ analysis/cmo/analyze.py
@@ -10,8 +10,6 @@ def draw_muon_spectrum(myset, muon_corsika, muon_mupage, costh_cut=0.95, bins=np
 
     for i in range(2):
         muon, name = muon_ls[i], name_ls[i]
-        # Restrict muons to be in the upper surface
-        muon = muon.loc[muon.z>499]
         color = default_color_list[i]
 
         # Select down-going muons
@@ -20,7 +18,7 @@ def draw_muon_spectrum(myset, muon_corsika, muon_mupage, costh_cut=0.95, bins=np
         ary, weights = muon.energy.to_numpy(), muon.weight.to_numpy()
 
         # Considering sr in weight
-        # weights unit: GeV s-1 m-2 rad-1
+        # weights unit: GeV s-1 m-2 sr-1
         weights = weights / (2*math.pi*(1-costh_cut)) * ary
         bin_contents, bins = np.histogram(ary, bins=bins, weights=weights)
 
@@ -57,17 +55,67 @@ def draw_muon_spectrum(myset, muon_corsika, muon_mupage, costh_cut=0.95, bins=np
     pc.draw_ratio(f'Mupage / CORSIKA', draw_error=True)
     pc.apply_settings(ratio_ylim=(0.1, 10), if_legend=False)
     pc.ax_ratio.set_yscale('log')
-    legend_loc = pc.legend_loc if hasattr(pc, 'legend_loc') else None
+    pc.ax.legend(fontsize=7, loc=None)
+    pc.savefig()
+
+
+def draw_muon_costh(myset, muon_corsika, muon_mupage, bins=np.linspace(0,1,41)):
+    muon_ls = [muon_corsika, muon_mupage]
+    name_ls = ["CORSIKA", "MUPAGE"]
+
+    pc = RatioPlotContainer(xlabel=r'cos$\theta$', ylabel=r'$dN/d\Omega dS dt\ \ [s^{-1}m^{-2}sr^{-1}]$', logx=False, logy=True, figname=myset.save_dir + 'muon_costh.pdf')
+    for i in range(2):
+        muon, name = muon_ls[i], name_ls[i]
+        color = default_color_list[i]
+        ary, weights = -1*muon.nz.to_numpy(), muon.weight.to_numpy()
+
+        # Considering sr in weight
+        # weights unit: s-1 m-2 rad-1
+        weights = weights / (2*math.pi)
+        bin_contents, bins = np.histogram(ary, bins=bins, weights=weights)
+
+        # y_value: dN/dcosth/dOmega/dS/dt
+        y_value = bin_contents/np.diff(bins)
+        hist, bins, _ = pc.ax.hist(bins[:-1], bins=bins, weights=y_value, label=name, histtype="step", linewidth=2, color=color)
+
+        # Calculate errors
+        y_err2, bins = np.histogram(ary, bins=bins, weights=(weights)**2)
+        y_err = y_err2**0.5 / np.diff(bins)
+        pc.ax.errorbar((bins[1:]+bins[:-1])/2, y_value, yerr=y_err, fmt='none', color=color)
+        pc.insert_data(
+            x_values=(bins[:-1]+bins[1:])/2, 
+            y_value=hist, ary_index=i, label='flux', y_err=y_err, color=color
+        )
+    # apply settings & draw ratio plot
+    pc.draw_ratio(f'Mupage / CORSIKA', draw_error=True)
+    pc.apply_settings(ratio_ylim=(0.1, 10), if_legend=False)
+    pc.ax_ratio.set_yscale('log')
+    legend_loc = "upper left"
     pc.ax.legend(fontsize=7, loc=legend_loc)
     pc.savefig()
     
+def draw_bundle_var():
+    pass
+
+def restrict_muons(muon_list):
+    for i in range(len(muon_list)):
+        muon = muon_list[i]
+        # Restrict muons to be in the upper surface
+        muon = muon.loc[muon.z>499]
+        # Restrict muon energy to be 100~1e5 GeV
+        muon = muon.loc[(muon.energy>100) & (muon.energy<1e5)]
+        muon_list[i] = muon
+    return muon_list
+
 if __name__ == '__main__':
     ##corsika
     myset = GlobalSetting()
-    muon_corsika = myset.muon_c8
-    muon_mupage = myset.muon_mupage
+    muon_corsika, muon_mupage = restrict_muons([myset.muon_c8, myset.muon_mupage])
    
     draw_muon_spectrum(myset=myset, muon_corsika=muon_corsika, muon_mupage=muon_mupage)
+
+    # Draw costh
+    draw_muon_costh(myset=myset, muon_corsika=muon_corsika, muon_mupage=muon_mupage)
 
     exit(0)
     plots = myset.plots
