0749f73cccb3cac7606429b492b8ea6aa4335da9
cmo <fourty2@126.com>
2024-04-07 16:33:33 +0800

    Draw bundle variables

--- analysis/cmo/analyze.py
+++ analysis/cmo/analyze.py
@@ -94,8 +94,53 @@ def draw_muon_costh(myset, muon_corsika, muon_mupage, bins=np.linspace(0,1,41)):
     pc.ax.legend(fontsize=7, loc=legend_loc)
     pc.savefig()
     
-def draw_bundle_var():
-    pass
+
+def draw_bundle_var(myset, muon_corsika, muon_mupage):
+    # Draw bundle variables
+    muon_ls = [muon_corsika, muon_mupage]
+    name_ls = ["CORSIKA", "MUPAGE"]
+
+    # Define vars to draw
+    plots = {
+        'multiplicity': RatioPlotContainer(xlabel='Multiplicity, m', ylabel=r'$dN/dm dS dt\ \ [s^{-1}m^{-2}]$', logx=False, logy=True, figname=myset.save_dir + 'bundle_multiplicity.pdf', bins=np.linspace(1, 40, 40), functor='count') ,
+        'bund_energy': RatioPlotContainer(xlabel=r'Bundle Energy [GeV]', ylabel=r'$EdN/dE dS dt\ \ [s^{-1}m^{-2}]$', logx=True, logy=True, figname=myset.save_dir + 'bundle_energy.pdf', bins=np.logspace(2, 6, 41), functor='sum') ,
+    }
+
+    for var, pc in plots.items():
+        bins = pc.bins
+        functor = pc.functor
+        for i, muon in enumerate(muon_ls):
+            color = default_color_list[i]
+            ary = muon.groupby('shower')['energy'].agg(functor).to_numpy()
+            weights = muon.groupby('shower')['weight'].first().to_numpy()
+            if 'energy' in var:
+                weights = weights * ary
+
+            # Insert array
+            bin_contents, bins = np.histogram(ary, bins=bins, weights=weights)
+
+            # y_value: dN/dx/dS/dt
+            y_value = bin_contents/np.diff(bins)
+            hist, bins, _ = pc.ax.hist(bins[:-1], bins=bins, weights=y_value, label=name_ls[i], histtype="step", linewidth=2, color=color)
+
+            # Calculate errors
+            y_err2, bins = np.histogram(ary, bins=bins, weights=(weights)**2)
+            y_err = y_err2**0.5 / np.diff(bins)
+            pc.ax.errorbar((bins[1:]+bins[:-1])/2, y_value, yerr=y_err, fmt='none', color=color)
+            pc.insert_data(
+                x_values=(bins[:-1]+bins[1:])/2, 
+                y_value=hist, ary_index=i, label='flux', y_err=y_err, color=color
+            )
+
+        # apply settings & draw ratio plot
+        pc.draw_ratio(f'Mupage / CORSIKA', draw_error=True)
+        pc.apply_settings(ratio_ylim=(0.1, 10), if_legend=False)
+        pc.ax_ratio.set_yscale('log')
+        legend_loc = None 
+        pc.ax.legend(fontsize=7, loc=legend_loc)
+        pc.savefig()
+
+
 
 def restrict_muons(muon_list):
     for i in range(len(muon_list)):
@@ -117,6 +162,8 @@ if __name__ == '__main__':
     # Draw costh
     draw_muon_costh(myset=myset, muon_corsika=muon_corsika, muon_mupage=muon_mupage)
 
+    # Draw boundle variables
+    draw_bundle_var(myset=myset, muon_corsika=muon_corsika, muon_mupage=muon_mupage)
     exit(0)
     plots = myset.plots
     variables = list(plots.keys())
