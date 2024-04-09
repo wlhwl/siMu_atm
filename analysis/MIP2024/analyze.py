078c28d6de54a381343a5e830a05a56e4af41ab7
cmo <fourty2@126.com>
2024-04-09 22:12:17 +0800

    compare models

--- analysis/MIP2024/analyze.py
+++ analysis/MIP2024/analyze.py
@@ -7,11 +7,11 @@ import copy
 def draw_muon_distribution(myset, muon):
     plots = {
         'energy': PlotContainer(logx=True, logy=True, xlabel=r'$E_\mu$ [GeV]', ylabel=rf'$E_\mu dN/dE$ [$m^{{-2}}s^{{-1}}sr^{{-1}}$]', 
-            title='', figname=myset.plot_dir+"muon_spectrum.pdf", bins = np.logspace(2, 5, 41), var_name='energy', weight_scale_cz=1),
+            title='', figname=myset.plot_dir+"muon_spectrum.pdf", bins = np.logspace(2, 5, 31), var_name='energy', weight_scale_cz=1, ylim=(1e-11, 1e-4)),
         'vertical energy spectrum': PlotContainer(logx=True, logy=True, xlabel=r'Vertical Muon $E_\mu$ [GeV]', ylabel=rf'$E_\mu dN/dE$ [$m^{{-2}}s^{{-1}}sr^{{-1}}$]', 
-            title='', figname=myset.plot_dir+"muon_spectrum_vertical.pdf", bins = np.logspace(2, 5, 41), czcut=0.95, var_name='energy', weight_scale_cz=1/0.05),
+            title='', figname=myset.plot_dir+"muon_spectrum_vertical.pdf", bins = np.logspace(2, 5, 31), czcut=0.95, var_name='energy', weight_scale_cz=1/0.05, ylim=(1e-11, 1e-4)),
         'nz': PlotContainer(logx=False, logy=True, xlabel=r'$cos(\theta)$', ylabel=rf'$dN/dcos\theta$ [$m^{{-2}}s^{{-1}}sr^{{-1}}$]', 
-            title='', figname=myset.plot_dir+"muon_cosz.pdf", bins = np.linspace(-1, 0, 41), var_name='nz', weight_scale_cz=1) 
+            title='', figname=myset.plot_dir+"muon_cosz.pdf", bins = np.linspace(-1, 0, 31), var_name='nz', weight_scale_cz=1) 
     }
 
     # Loop to draw
@@ -99,29 +99,52 @@ def draw_bundle_var(myset, muon_corsika, muon_mupage):
         pc.ax.legend(fontsize=7, loc=legend_loc)
         pc.savefig()
 
-def restrict_muons(muon_list):
-    for i in range(len(muon_list)):
-        muon = muon_list[i]
-        # Restrict muons to be in the upper surface
-        muon = muon.loc[muon.z>499]
-        # Restrict muon energy to be 100~1e5 GeV
-        muon = muon.loc[(muon.energy>100) & (muon.energy<1e5)]
-        muon_list[i] = muon
-    return muon_list
+def compare_flux_models(myset, muon, model_name_list=["GSF", "GST3", "PolyGonato"]):
+    pc = RatioPlotContainer(logx=True, logy=True, xlabel=r'$E_\mu$ [GeV]', ylabel=rf'$E_\mu dN/dE$ [$m^{{-2}}s^{{-1}}sr^{{-1}}$]', 
+            title='', figname=myset.plot_dir+"muon_spectrum_all_models.pdf", var_name='energy', weight_scale_cz=1)
+    bins = np.logspace(2,5,31)
+    x_values = (bins[1:]+bins[:-1])/2
+    ary = muon['energy'].to_numpy()
+    nominal_model = 'GSF'
+    for i, model_name in enumerate(model_name_list):
+        color = default_color_list[i]
+        weight = muon['weight_'+model_name].to_numpy() / (2*math.pi) * ary
+
+        # Draw result
+        bin_contents, bins = np.histogram(ary, bins=bins, weights=weight)
+        y_values = bin_contents/np.diff(bins)
+        pc.ax_main.step(x_values, y_values, label=model_name, where='mid', linewidth=2, color=color)
+        # Calculate errors
+        y_err2, bins = np.histogram(ary, bins=bins, weights=(weight)**2)
+        y_err = y_err2**0.5 / np.diff(bins)
+        pc.ax_main.errorbar(x_values, y_values, yerr=y_err, fmt='none', color=color)
+
+        # Save nominal result
+        if model_name == nominal_model:
+            # Fill in nominal
+            for i, fill_nomnial_model_name in enumerate(model_name_list):
+                if fill_nomnial_model_name != nominal_model:
+                    pc.insert_data(
+                        x_values=x_values, y_value=y_values, ary_index=0, label=fill_nomnial_model_name, y_err=y_err
+                    )
+        else:
+            pc.insert_data(
+                x_values=x_values, y_value=y_values, ary_index=1, label=model_name, y_err=y_err, color=color
+            )
+    
+    pc.draw_ratio(f'Ratio to '+nominal_model, draw_error=True)
+    pc.apply_settings(ratio_ylim=(0.2, 1.2), if_legend=True)
+    pc.savefig()
+
 
 if __name__ == '__main__':
     ##corsika
     myset = GlobalSetting()
-    muon_corsika, muon_mupage = restrict_muons([myset.muon_c8, myset.muon_mupage])
+    muon_corsika, muon_mupage = myset.muon_c8, myset.muon_mupage
    
     draw_muon_distribution(myset=myset, muon=muon_corsika)
-    # draw_muon_spectrum(myset=myset, muon_corsika=muon_corsika, muon_mupage=muon_mupage)
 
-    # # Draw costh
-    # draw_muon_costh(myset=myset, muon_corsika=muon_corsika, muon_mupage=muon_mupage)
-
-    # # Draw boundle variables
-    # draw_bundle_var(myset=myset, muon_corsika=muon_corsika, muon_mupage=muon_mupage)
+    compare_flux_models(myset=myset, muon=muon_corsika)
 
     # Draw primary spectrum
     pc =  PlotContainer(xlabel=r'Primary Energy [GeV]', ylabel=r'$EdN/dE dS dt\ \ [s^{-1}m^{-2}]$', logx=True, logy=True, figname=myset.plot_dir + 'primary_spectrum.pdf') 
