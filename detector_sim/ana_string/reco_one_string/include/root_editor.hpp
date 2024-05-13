c08cfa3741bbfdadaf4bca3f74c1f2c4cfdd9680
Weilun Huang <huangweilun@mail.sdu.edu.cn>
2024-05-13 16:16:13 +0800

    reshape the project

---
+++ detector_sim/ana_string/reco_one_string/include/root_editor.hpp
@@ -0,0 +1,45 @@
+#ifndef ROOT_EDITOR_HPP
+#define ROOT_EDITOR_HPP 1
+
+#include "TBranch.h"
+#include "TFile.h"
+#include <iostream>
+
+class root_editor {
+public:
+    root_editor();
+    ~root_editor();
+    static TTree* load_tree(TFile* , std::string );
+
+    template <typename U>
+    static int branches_getentry(U& struct_name, int i){
+        for (TBranch* branch : struct_name.branches){
+            branch->GetEntry(i);
+        }
+        return i;
+    };
+
+    template <typename T>
+    static TBranch* load_branch(TTree* tree, std::string branch_name, T branch_data){
+        TBranch* branch = nullptr;
+        if(tree == nullptr){
+            std::cerr << "Tree is null" << std::endl;
+            return nullptr;
+        }
+        if(tree->FindBranch(branch_name.c_str())){
+            branch = tree->GetBranch(branch_name.c_str());
+            if (branch == nullptr) {
+                std::cerr << "Branch " << branch_name << " not found in tree" << std::endl;
+                return nullptr;
+            }
+            branch->SetAddress(branch_data);
+        }else{
+            branch = tree->Branch(branch_name.c_str(), branch_data);
+        }
+        return branch;
+    }
+
+
+};
+
+#endif
